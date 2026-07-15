b9a1ee3ad5d8b4f8ada0cbb29de43219f604a1b4
Mohammad mahdi <232824794+mahdikimiyaei@users.noreply.github.com>
2026-07-15 22:55:32 +0330

    update files

--- script.js
+++ script.js
@@ -1,307 +1,1073 @@
+
 // ===============================
 // LOGIN SYSTEM
 // ===============================
 
+
 const loginForm = document.querySelector("#loginForm");
 
-if (loginForm) {
-  loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
 
-    const username = document.querySelector("#username").value;
+if(loginForm){
 
-    const password = document.querySelector("#password").value;
 
-    if (username.length >= 3 && password.length >= 4) {
-      const user = {
-        username: username,
+loginForm.addEventListener("submit",(e)=>{
 
-        login: true,
-      };
 
-      localStorage.setItem("user", JSON.stringify(user));
+e.preventDefault();
 
-      window.location.href = "shop.html";
-    } else {
-      document.querySelector("#loginError").innerText =
-        "نام کاربری یا رمز عبور صحیح نیست";
-    }
-  });
+
+
+const username =
+document.querySelector("#username").value;
+
+
+
+const password =
+document.querySelector("#password").value;
+
+
+
+const phoneRegex = /^09\d{9}$/;
+
+
+
+if(phoneRegex.test(username) && password.length >= 4){
+
+
+
+const user = {
+
+id: username,
+
+username: username,
+
+login:true
+
+};
+
+
+
+// پاک کردن کاربر قبلی
+
+localStorage.removeItem(
+"currentUser"
+);
+
+
+
+// ذخیره کاربر جدید
+
+localStorage.setItem(
+"currentUser",
+JSON.stringify(user)
+);
+
+
+
+
+
+const userData =
+localStorage.getItem(
+`userData_${username}`
+);
+
+
+
+if(!userData){
+
+
+localStorage.setItem(
+
+`userData_${username}`,
+
+JSON.stringify({
+
+cart:[],
+
+orders:[]
+
+})
+
+);
+
+
 }
 
+
+
+window.location.replace(
+"shop.html"
+);
+
+
+
+}
+
+else{
+
+
+document.querySelector("#loginError").innerText =
+"شماره موبایل یا رمز عبور اشتباه است";
+
+
+}
+
+
+
+});
+
+
+}
+
+
+
+
+
+
+
+
 // ===============================
-// PROTECT SHOP PAGE
+// CHECK LOGIN
 // ===============================
 
-if (window.location.pathname.includes("shop.html")) {
-  const user = JSON.parse(localStorage.getItem("user"));
 
-  if (!user || !user.login) {
-    window.location.href = "login.html";
-  }
+
+if(window.location.pathname.includes("shop.html")){
+
+
+const user =
+JSON.parse(
+localStorage.getItem("currentUser")
+);
+
+
+
+if(!user || !user.login){
+
+
+window.location.replace(
+"index.html"
+);
+
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+// ===============================
+// CURRENT USER
+// ===============================
+
+
+
+let currentUser =
+JSON.parse(
+localStorage.getItem("currentUser")
+);
+
+
+
+
+
+
+
+
+
+// ===============================
+// LOAD USER DATA
+// ===============================
+
+
+let cart=[];
+
+let orders=[];
+
+
+
+function loadUserData(){
+
+
+
+currentUser =
+JSON.parse(
+localStorage.getItem("currentUser")
+);
+
+
+
+if(!currentUser){
+
+
+cart=[];
+
+orders=[];
+
+return;
+
+
+}
+
+
+
+const data =
+
+JSON.parse(
+
+localStorage.getItem(
+
+`userData_${currentUser.id}`
+
+)
+
+);
+
+
+
+cart =
+data?.cart || [];
+
+
+
+orders =
+data?.orders || [];
+
+
+
+}
+
+
+
+loadUserData();
+
+
+
+
+
+
+
+
+
+// ===============================
+// SAVE USER DATA
+// ===============================
+
+
+function saveUserData(){
+
+
+
+if(!currentUser)
+return;
+
+
+
+localStorage.setItem(
+
+`userData_${currentUser.id}`,
+
+JSON.stringify({
+
+cart,
+
+orders
+
+})
+
+);
+
+
 }
 
 // ===============================
 // LOGOUT
 // ===============================
 
-const logoutBtn = document.querySelector("#logoutBtn");
 
-if (logoutBtn) {
-  logoutBtn.addEventListener("click", () => {
-    localStorage.removeItem("user");
+const logoutBtn =
+document.querySelector("#logoutBtn");
 
-    localStorage.removeItem("cart");
 
-    window.location.href = "index.html";
-  });
+
+if(logoutBtn){
+
+
+logoutBtn.addEventListener("click",()=>{
+
+
+
+saveUserData();
+
+
+
+localStorage.removeItem(
+"currentUser"
+);
+
+
+
+window.location.replace(
+"index.html"
+);
+
+
+
+});
+
+
 }
+
+
+
+
+
+
+
+
 
 // ===============================
-// CART SYSTEM
+// CART
 // ===============================
 
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-const cartItems = document.querySelector("#cartItems");
 
-const totalPrice = document.querySelector("#totalPrice");
+const cartItems =
+document.querySelector("#cartItems");
 
-function saveCart() {
-  localStorage.setItem("cart", JSON.stringify(cart));
+
+
+const totalPrice =
+document.querySelector("#totalPrice");
+
+
+
+
+
+
+
+function renderCart(){
+
+
+
+if(!cartItems)
+return;
+
+
+
+cartItems.innerHTML="";
+
+
+
+let total=0;
+
+
+
+cart.forEach((item,index)=>{
+
+
+
+total += item.price;
+
+
+
+cartItems.innerHTML += `
+
+
+<div class="cart-item">
+
+
+<span>
+${item.name}
+</span>
+
+
+
+<span>
+
+${item.price.toLocaleString()}
+
+تومان
+
+</span>
+
+
+
+<button onclick="removeCart(${index})">
+
+حذف
+
+</button>
+
+
+
+</div>
+
+
+`;
+
+
+
+});
+
+
+
+if(totalPrice){
+
+totalPrice.innerText =
+total.toLocaleString();
+
 }
 
-function renderCart() {
-  if (!cartItems) return;
-
-  cartItems.innerHTML = "";
-
-  let total = 0;
-
-  cart.forEach((item, index) => {
-    total += item.price;
-
-    const div = document.createElement("div");
-
-    div.className = "cart-item";
-
-    div.innerHTML = `
-
-        <span>
-        ${item.name}
-        </span>
 
 
-        <span>
-        ${item.price.toLocaleString()}
-        تومان
-        </span>
-
-
-        <button onclick="removeCart(${index})">
-        حذف
-        </button>
-
-        `;
-
-    cartItems.appendChild(div);
-  });
-
-  totalPrice.innerText = total.toLocaleString();
 }
 
-function removeCart(index) {
-  cart.splice(index, 1);
 
-  saveCart();
 
-  renderCart();
+
+
+
+
+function removeCart(index){
+
+
+
+cart.splice(index,1);
+
+
+
+saveUserData();
+
+
+
+renderCart();
+
+
+
 }
 
-window.removeCart = removeCart;
+
+
+window.removeCart =
+removeCart;
+
+
+
+
+
+
+
+
 
 // ===============================
 // ADD PRODUCT
 // ===============================
 
-const addButtons = document.querySelectorAll(".add-cart");
 
-addButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const product = {
-      name: button.dataset.name,
 
-      price: Number(button.dataset.price),
-    };
+const addButtons =
+document.querySelectorAll(".add-cart");
 
-    cart.push(product);
 
-    saveCart();
 
-    renderCart();
+addButtons.forEach(button=>{
 
-    showMessage("محصول به سبد خرید اضافه شد");
-  });
+
+button.addEventListener("click",()=>{
+
+
+
+const product = {
+
+
+id:Date.now(),
+
+
+
+name:
+button.dataset.name,
+
+
+
+price:
+Number(
+button.dataset.price
+)
+
+
+
+};
+
+
+
+
+cart.push(product);
+
+
+
+saveUserData();
+
+
+
+renderCart();
+
+
+
+showMessage(
+"محصول به سبد خرید اضافه شد"
+);
+
+
+
 });
 
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 // ===============================
-// TOAST MESSAGE
+// MESSAGE
 // ===============================
 
-function showMessage(text) {
-  const toast = document.createElement("div");
 
-  toast.innerText = text;
 
-  toast.style.position = "fixed";
+function showMessage(text){
 
-  toast.style.bottom = "30px";
 
-  toast.style.left = "50%";
 
-  toast.style.transform = "translateX(-50%)";
+const toast =
+document.createElement("div");
 
-  toast.style.background = "#22c55e";
 
-  toast.style.color = "white";
 
-  toast.style.padding = "15px 25px";
+toast.innerText=text;
 
-  toast.style.borderRadius = "15px";
 
-  toast.style.zIndex = "999";
 
-  toast.style.animation = "fade .4s";
+toast.style.position="fixed";
 
-  document.body.appendChild(toast);
+toast.style.bottom="30px";
 
-  setTimeout(() => {
-    toast.remove();
-  }, 2500);
+toast.style.left="50%";
+
+toast.style.transform=
+"translateX(-50%)";
+
+toast.style.background="#22c55e";
+
+toast.style.color="white";
+
+toast.style.padding="15px 25px";
+
+toast.style.borderRadius="15px";
+
+toast.style.zIndex="999";
+
+
+
+document.body.appendChild(toast);
+
+
+
+setTimeout(()=>{
+
+
+toast.remove();
+
+
+},2000);
+
+
+
 }
 
+
+
+
+
+
+
+
+
+// ===============================
+// DELIVERY OPTIONS
+// ===============================
+
+
+const deliveryTime =
+document.querySelector("#deliveryTime");
+
+
+
+function createDeliveryOptions(){
+
+
+
+if(!deliveryTime)
+return;
+
+
+
+deliveryTime.innerHTML = `
+
+<option value="">
+
+انتخاب زمان ارسال
+
+</option>
+
+`;
+
+
+
+const now =
+new Date();
+
+
+
+const timeSlots = [
+
+
+"10:00 تا 12:00",
+
+"12:00 تا 14:00",
+
+"14:00 تا 16:00",
+
+"16:00 تا 18:00",
+
+"18:00 تا 20:00",
+
+"20:00 تا 22:00"
+
+
+];
+
+
+
+for(let day = 3; day <= 7; day++){
+
+
+
+const date =
+new Date();
+
+
+
+date.setDate(
+now.getDate() + day
+);
+
+
+
+
+const dateText =
+date.toLocaleDateString("fa-IR");
+
+
+
+
+timeSlots.forEach(time=>{
+
+
+
+const option =
+document.createElement("option");
+
+
+
+option.value =
+`${dateText} - ${time}`;
+
+
+
+option.innerText =
+`${dateText} - ${time}`;
+
+
+
+deliveryTime.appendChild(option);
+
+
+
+});
+
+
+}
+
+
+}
+
+
+
+createDeliveryOptions();
 // ===============================
 // CHECKOUT
 // ===============================
 
-const checkoutForm = document.querySelector("#checkoutForm");
 
-if (checkoutForm) {
-  checkoutForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+const checkoutForm =
+document.querySelector("#checkoutForm");
 
-    if (cart.length === 0) {
-      alert("سبد خرید خالی است");
 
-      return;
-    }
 
-    const order = {
-      name: document.querySelector("#fullname").value,
+if(checkoutForm){
 
-      email: document.querySelector("#email").value,
 
-      address: document.querySelector("#address").value,
+checkoutForm.addEventListener("submit",(e)=>{
 
-      postal: document.querySelector("#postal").value,
 
-      products: cart,
+e.preventDefault();
 
-      total: cart.reduce((sum, item) => sum + item.price, 0),
-    };
 
-    localStorage.setItem("order", JSON.stringify(order));
 
-    showOrder(order);
 
-    cart = [];
+if(cart.length===0){
 
-    saveCart();
 
-    renderCart();
+alert(
+"سبد خرید خالی است"
+);
 
-    checkoutForm.reset();
-  });
+
+return;
+
+
 }
 
+
+
+
+
+
+
+const order={
+
+
+
+id:Date.now(),
+
+
+
+userId:
+currentUser.id,
+
+
+
+name:
+document.querySelector("#fullname").value,
+
+
+
+email:
+document.querySelector("#email").value,
+
+
+
+address:
+document.querySelector("#address").value,
+
+
+
+postal:
+document.querySelector("#postal").value,
+
+
+
+products:
+[...cart],
+
+
+
+
+total:
+
+cart.reduce(
+
+(sum,item)=>
+
+sum + item.price,
+
+0
+
+),
+
+
+
+date:
+
+new Date()
+.toLocaleString("fa-IR"),
+
+
+
+deliveryTime:
+
+document.querySelector("#deliveryTime").value
+
+
+
+};
+
+
+
+
+
+
+
+orders.push(order);
+
+
+
+// خالی کردن سبد
+
+cart=[];
+
+
+
+saveUserData();
+
+
+
+renderCart();
+
+
+
+showOrders();
+
+
+
+checkoutForm.reset();
+
+
+
+showMessage(
+"سفارش ثبت شد"
+);
+
+
+
+});
+
+
+}
+
+
+
+
+
+
+
+
+
 // ===============================
-// SHOW ORDER
+// SHOW ORDERS
 // ===============================
 
-function showOrder(order) {
-  const result = document.querySelector("#orderResult");
 
-  if (!result) return;
-
-  result.innerHTML = `
-
-<div class="success">
-
-✔ سفارش با موفقیت ثبت شد
-
-</div>
+function showOrders(){
 
 
-<br>
+
+const result =
+document.querySelector("#orderResult");
 
 
-<strong>
+
+if(!result)
+return;
+
+
+
+result.innerHTML="";
+
+
+
+if(orders.length===0){
+
+
+
+result.innerHTML =
+"هنوز سفارشی ثبت نشده است";
+
+return;
+
+
+}
+
+
+
+
+
+
+
+orders.forEach(order=>{
+
+
+result.innerHTML += `
+
+
+<div class="order-card">
+
+
+<h3>
+✔ سفارش ثبت شده
+</h3>
+
+
+
 نام:
-</strong>
 
 ${order.name}
 
-
 <br>
 
 
-<strong>
+
 ایمیل:
-</strong>
 
 ${order.email}
 
-
 <br>
 
 
-<strong>
+
 آدرس:
-</strong>
 
 ${order.address}
 
-
 <br>
 
 
-<strong>
-کدپستی:
-</strong>
+
+کد پستی:
 
 ${order.postal}
 
 
+
+<br><br>
+
+
+
+محصولات:
+
+
 <br>
 
 
-<strong>
-مبلغ نهایی:
-</strong>
+
+${
+
+order.products.map(item=>`
+
+
+${item.name}
+
+-
+
+${item.price.toLocaleString()}
+
+تومان
+
+<br>
+
+
+`).join("")
+
+}
+
+
+
+
+
+<br>
+
+
+
+مبلغ:
 
 ${order.total.toLocaleString()}
+
 تومان
 
 
+
+
+
+<br>
+
+
+
+تاریخ ثبت:
+
+${order.date}
+
+
+
+
+
+
+<br>
+
+
+
+زمان ارسال:
+
+${order.deliveryTime}
+
+
+
+
+
+</div>
+
+
+<hr>
+
+
 `;
+
+
+
+});
+
+
 }
+
+
+
+
+
+
+
+
+
+// ===============================
+// START
+// ===============================
+
+
 
 renderCart();
 
-// نمایش سفارش قبلی بعد از رفرش
 
-const oldOrder = JSON.parse(localStorage.getItem("order"));
-
-if (oldOrder) {
-  showOrder(oldOrder);
-}
+showOrders();
